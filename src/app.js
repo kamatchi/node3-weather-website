108c7fb3791aee2ae826bea7fbbc497513799719
kamatchi <kamatchi.pandiarajan@pwc.com>
2020-05-10 02:20:57 +0530

    Setup app for heroku

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 // Define path for Express config
 const publicDirectoryPath = path.join(__dirname, '../public');
@@ -90,6 +91,6 @@ app.get('*', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('App is running on port 3000!');
+app.listen(port, () => {
+  console.log(`App is running on port ${port}!`);
 });
